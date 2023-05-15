3f9e7f99fbafd71e5ca295e32acedeff118b36db
Pablo GP <garciaplaza.pablo@gmail.com>
2023-05-15 23:24:22 +0200

    Created page with null return for single post

---
+++ app/posts/[id]/page.js
@@ -0,0 +1,13 @@
+// import ListOfPost from './ListOfPost'
+
+export default function post() {
+  // const { id } = params
+
+  return null
+  // <>
+
+  //   {/* <ListOfPost id={id} /> */}
+  //   {/* <p>this is post nr {id}</p> */}
+  //   {/* {children} */}
+  // </>
+}
